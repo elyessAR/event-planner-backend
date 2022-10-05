c953a257a36c8419fab4084a206c55506fa8483d
Elyes Arifa <earifa@resqsoft.com>
2022-10-05 03:16:00 +0100

    added changes

---
+++ controllers/posts.js
@@ -0,0 +1,63 @@
+import PostMessage from '../models/PostMessage.js';
+import mongoose from 'mongoose';
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const { title, message, selectedFile, creator, tags } = req.body;
+  const newPostMessage = new PostMessage({
+    title,
+    message,
+    selectedFile,
+    creator,
+    tags,
+  });
+
+  try {
+    await newPostMessage.save();
+    res.status(201).json(newPostMessage);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { title, message, creator, selectedFile, tags } = req.body;
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send('no post with that id');
+  const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+  await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+  res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send('no post with that id');
+  // const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+  await PostMessage.findByIdAndRemove(id);
+  res.json({ message: 'Event Deleted Successfully.' });
+};
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send('no post with that id');
+  // const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { likeCount: post.likeCount + 1 },
+    { new: true }
+  );
+
+  res.json(updatedPost);
+};
--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import postRoutes from "./routes/posts.js";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
-
-app.use("/posts", postRoutes);
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+dotenv.config();
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+app.use('/posts', postRoutes);
+// app.post("/posts", (req, res) => {
+//   console.log(req.body);
+//   res.status(201).json(req.body);
+// });
 
-const CONNECTION_URL =
-  "mongodb+srv://dvl123:jbDd3qoeDUWsFjzz@cluster0.ztygorh.mongodb.net/?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://dvl123:jbDd3qoeDUWsFjzz@cluster0.ztygorh.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
   )
